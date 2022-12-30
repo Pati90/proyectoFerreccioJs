7d25fe1d2a8d672178a6e284ec64b24b74fa8cb8
Pablo Ferreccio <ferrecciop@gmail.com>
2022-12-29 23:04:50 -0300

    Proyecto Final Javascrtip

---
+++ js/main.js
@@ -0,0 +1,399 @@
+// STICKERS
+const productos = [
+    //LOS SIMPSONS
+    {
+        id: "simpsons-01",
+        titulo: "Simpsons 01",
+        imagen: "./images/simpsons/1.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-02",
+        titulo: "Simpsons 02",
+        imagen: "./images/simpsons/2.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-03",
+        titulo: "Simpsons 03",
+        imagen: "./images/simpsons/3.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-04",
+        titulo: "Simpsons 04",
+        imagen: "./images/simpsons/4.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-05",
+        titulo: "Simpsons 05",
+        imagen: "./images/simpsons/5.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-06",
+        titulo: "Simpsons 06",
+        imagen: "./images/simpsons/6.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-07",
+        titulo: "Simpsons 07",
+        imagen: "./images/simpsons/7.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-08",
+        titulo: "Simpsons 08",
+        imagen: "./images/simpsons/8.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-09",
+        titulo: "Simpsons 09",
+        imagen: "./images/simpsons/9.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    {
+        id: "simpsons-10",
+        titulo: "Simpsons 10",
+        imagen: "./images/simpsons/10.jpg",
+        categoria: {
+            nombre: "Simpsons",
+            id: "simpsons"
+        },
+        precio: 150
+    },
+    // Fútbol
+    {
+        id: "futbol-01",
+        titulo: "Fútbol 01",
+        imagen: "./images/futbol/1.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-02",
+        titulo: "Fútbol 02",
+        imagen: "./images/futbol/2.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-03",
+        titulo: "Fútbol 03",
+        imagen: "./images/futbol/3.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-04",
+        titulo: "Fútbol 04",
+        imagen: "./images/futbol/4.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-05",
+        titulo: "Fútbol 05",
+        imagen: "./images/futbol/5.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-06",
+        titulo: "Fútbol 06",
+        imagen: "./images/futbol/6.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-07",
+        titulo: "Fútbol 07",
+        imagen: "./images/futbol/7.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-08",
+        titulo: "Fútbol 08",
+        imagen: "./images/futbol/8.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-09",
+        titulo: "Fútbol 09",
+        imagen: "./images/futbol/9.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    {
+        id: "futbol-10",
+        titulo: "Fútbol 10",
+        imagen: "./images/futbol/10.jpg",
+        categoria: {
+            nombre: "Fútbol",
+            id: "futbol"
+        },
+        precio: 200
+    },
+    // Signos
+    {
+        id: "signo-01",
+        titulo: "Signos 01",
+        imagen: "./images/signos/1.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-02",
+        titulo: "Signos 02",
+        imagen: "./images/signos/2.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-03",
+        titulo: "Signos 03",
+        imagen: "./images/signos/3.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-04",
+        titulo: "Signos 04",
+        imagen: "./images/signos/4.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-05",
+        titulo: "Signos 05",
+        imagen: "./images/signos/5.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-06",
+        titulo: "Signos 06",
+        imagen: "./images/signos/6.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-07",
+        titulo: "Signos 07",
+        imagen: "./images/signos/7.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-08",
+        titulo: "Signos 08",
+        imagen: "./images/signos/8.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-09",
+        titulo: "Signos 09",
+        imagen: "./images/signos/9.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    },
+    {
+        id: "signo-10",
+        titulo: "Signos 10",
+        imagen: "./images/signos/10.jpg",
+        categoria: {
+            nombre: "Signos",
+            id: "signos"
+        },
+        precio: 50
+    }
+];
+
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}       
